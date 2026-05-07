76ece92f79269bfd561a12bf4a967cbb2c5f0f44
ggaryfallos-gg <g.garyfallos@gmail.com>
2026-05-07 09:58:00 +0300

    Update map.py

--- map.py
+++ map.py
@@ -679,11 +679,10 @@ elif app_mode == "📊 Admin Dashboard":
                   st.rerun()
               except Exception as e:
                                     st.error(f"❌ Σφάλμα API: Βεβαιωθείτε ότι το Sheet είναι Shared με δικαιώματα Editor. Λεπτομέρειες: {e}")
-              
           else:
               st.info("Δεν υπάρχουν εκκρεμείς παραλαβές στο σύστημα.")
     except Exception as ex:
-                          st.error(f"Δεν ήταν δυνατή η φόρτωση των παραλαβών: {e}")
+      st.error(f"Δεν ήταν δυνατή η φόρτωση των παραλαβών: {e}")
 
 
 
